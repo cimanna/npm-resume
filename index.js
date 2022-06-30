cd31e9cdd9af396ec443cb2fb7ca67641b76cb21
Christine Manna <cimanna@gmail.com>
2022-06-30 18:46:38 -0400

    resume commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+#! /usr/bin/env node
+
+
+const fs = require('fs')
+
+fs.readFile('npm-resume' + '/info.json', 'utf8', function(err, data) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data)
+        return data
+    }
+})
+
